a314b5f31367a221ccc0b084ca80e05c06cdcf3b
dungbaoviec123 <dungtran160820051508@gmail.com>
2026-01-02 10:39:51 +0700

    feat(App): Add SafeAreaView

--- App.tsx
+++ App.tsx
@@ -3,12 +3,23 @@ import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from './src/context/ThemeContext';
 import HomePage from './src/screens/HomePage/HomePage';
 import './src/i18n/config';
+// Import thư viện (bạn đã có dòng này rồi)
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <HomePage />
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    // 1. SafeAreaProvider phải nằm ở cấp cao nhất (root)
+    <SafeAreaProvider>
+      <ThemeProvider>
+        {/* 2. SafeAreaView bọc lấy nội dung hiển thị */}
+        {/* Quan trọng: Phải có style flex: 1 để nó chiếm toàn bộ màn hình */}
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'rgba(0, 102, 255, 0.7)' }}>
+          
+          <HomePage />
+          <StatusBar style="auto" />
+          
+        </SafeAreaView>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
